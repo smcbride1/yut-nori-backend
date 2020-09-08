fa4a7d7ca1070d6f52ab571370f28a0d4d003b82
Stephen McBride <stevemcbride3@gmail.com>
2020-09-08 09:49:02 -0400

    Updated create_round migration

--- db/migrate/20200908134330_create_round.rb
+++ db/migrate/20200908134330_create_round.rb
@@ -1,6 +1,10 @@
 class CreateRound < ActiveRecord::Migration[6.0]
   def change
     create_table :rounds do |t|
+      t.integer :match_id
+      t.integer :location
+
+      t.timestamps
     end
   end
 end
