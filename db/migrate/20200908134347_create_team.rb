2dbd788aa93dde357d634894dbcea61156444984
Stephen McBride <stevemcbride3@gmail.com>
2020-09-08 09:51:47 -0400

    Updated create_team migration

--- db/migrate/20200908134347_create_team.rb
+++ db/migrate/20200908134347_create_team.rb
@@ -1,6 +1,9 @@
 class CreateTeam < ActiveRecord::Migration[6.0]
   def change
     create_table :teams do |t|
+      t.integer :match_id
+      t.integer :current_player
+      t.integer :team_leader
     end
   end
 end
