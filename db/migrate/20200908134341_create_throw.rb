304940f679ca3ad73dfcc023d74d7e1ce9a19d50
Stephen McBride <stevemcbride3@gmail.com>
2020-09-08 09:50:22 -0400

    Updated create_throw migration

--- db/migrate/20200908134341_create_throw.rb
+++ db/migrate/20200908134341_create_throw.rb
@@ -1,6 +1,11 @@
 class CreateThrow < ActiveRecord::Migration[6.0]
   def change
     create_table :throws do |t|
+      t.integer :round_id
+      t.integer :score
+      t.boolean :can_rethrow
+
+      t.timestamps
     end
   end
 end
