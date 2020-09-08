5e390b9121a37819149d3711624d6108f03b57c1
Stephen McBride <stevemcbride3@gmail.com>
2020-09-08 10:14:07 -0400

    Updated matches_controller

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,2 +1,13 @@
 class MatchesController < ApplicationController
+    def show
+        Matches.find(params[:id])
+    end
+
+    def create
+        @match = Matches.create(match_params)
+    end
+
+    def match_params
+        params.require(:match).permit(:starting_team)
+    end
 end
