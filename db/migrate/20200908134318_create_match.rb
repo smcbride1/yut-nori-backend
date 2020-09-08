73f650b6ada9494388cf6ba82d176624c35b6983
Stephen McBride <stevemcbride3@gmail.com>
2020-09-08 09:47:04 -0400

    Updated create_match migration

--- db/migrate/20200908134318_create_match.rb
+++ db/migrate/20200908134318_create_match.rb
@@ -1,6 +1,10 @@
 class CreateMatch < ActiveRecord::Migration[6.0]
   def change
     create_table :matches do |t|
+      t.boolean :finished
+      t.starting_team :integer
+
+      t.timestamps
     end
   end
 end
